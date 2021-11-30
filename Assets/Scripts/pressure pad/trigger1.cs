1418e792b8056f87d7375831a6ff606c27755c2e
ChayaTung <chayapalm32867@gmail.com>
2021-11-30 04:52:55 +0000

    motion

--- Assets/Scripts/pressure pad/trigger1.cs
+++ Assets/Scripts/pressure pad/trigger1.cs
@@ -6,27 +6,43 @@ public class trigger1 : MonoBehaviour
 {
     [SerializeField]
     GameObject bridge1;
-    
- 
+    Vector3 startposition;
+    Vector3 endposition;
+
+    void Start()
+    {
+        startposition = bridge1.transform.position;
+        endposition = startposition + new Vector3(3, 0, 0);
+    }
+
     private void OnTriggerEnter(Collider other)
     {
-
-        movebridge(2f);
-           
+        StopAllCoroutines();
+        StartCoroutine(bridgemoveforward(50f));
 
     }
     private void OnTriggerExit(Collider other)
     {
-        bridge1.transform.position += new Vector3(-3, 0, 0);
-        
+        StopAllCoroutines();
+        StartCoroutine(bridgemovebackward(50f));
+      
     }
     
-    public IEnumerator movebridge(float time)
+    public IEnumerator bridgemoveforward(float time)
     {
-        Vector3 destination = bridge1.transform.position + new Vector3(3, 0, 0);
+        
         for (var t = 0f; t < 1; t += Time.deltaTime / time)
         {
-          bridge1.transform.position= Vector3.Lerp(bridge1.transform.position,destination, t);
+          bridge1.transform.position= Vector3.Lerp(bridge1.transform.position,endposition, t);
+            yield return null;
+        }
+    }
+    public IEnumerator bridgemovebackward(float time)
+    {
+       
+        for (var t = 0f; t < 1; t += Time.deltaTime / time)
+        {
+            bridge1.transform.position = Vector3.Lerp(bridge1.transform.position,startposition, t);
             yield return null;
         }
     }
--- Assets/Scripts/pressure pad/trigger2.cs
+++ Assets/Scripts/pressure pad/trigger2.cs
@@ -6,14 +6,44 @@ public class trigger2 : MonoBehaviour
 {
     [SerializeField]
     GameObject bridge2;
-    
+    Vector3 startposition;
+    Vector3 endposition;
+
+    void Start()
+    {
+        startposition = bridge2.transform.position;
+        endposition = startposition + new Vector3(-3, 0, 0);
+    }
+
     private void OnTriggerEnter(Collider other)
     {
-      
-            bridge2.transform.position += new Vector3(-3, 0, 0);
+        StopAllCoroutines();
+        StartCoroutine(bridgemoveforward(50f));
+
     }
     private void OnTriggerExit(Collider other)
     {
-        bridge2.transform.position += new Vector3(3, 0, 0);
+        StopAllCoroutines();
+        StartCoroutine(bridgemovebackward(50f));
+
+    }
+
+    public IEnumerator bridgemoveforward(float time)
+    {
+        
+        for (var t = 0f; t < 1; t += Time.deltaTime / time)
+        {
+            bridge2.transform.position = Vector3.Lerp(bridge2.transform.position, endposition, t);
+            yield return null;
+        }
+    }
+    public IEnumerator bridgemovebackward(float time)
+    {
+        
+        for (var t = 0f; t < 1; t += Time.deltaTime / time)
+        {
+            bridge2.transform.position = Vector3.Lerp(bridge2.transform.position,startposition, t);
+            yield return null;
+        }
     }
 }
