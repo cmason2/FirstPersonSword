1eaad8e799d669b2d88fe52a9b76d5a3cafbfe9c
ChayaTung <chayapalm32867@gmail.com>
2021-11-30 11:30:10 +0000

    spike

---
+++ Assets/Scripts/Interactable Objects/Spike/Spike_switch.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spike_switch : MonoBehaviour
+{
+    Animator animators;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animators = GetComponentInChildren<Animator>();
+
+    }
+
+    private void OnTriggerEnter(Collider collider)
+    {
+
+        animators.SetTrigger("closewall");
+
+
+
+    }
+}
