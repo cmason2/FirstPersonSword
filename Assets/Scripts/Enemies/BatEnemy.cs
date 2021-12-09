6be20ce194eacf7bcdceafb411463b4f248ab651
Charlie Mason <56030635+cmason2@users.noreply.github.com>
2021-12-09 13:58:04 +0000

    Fixed spike room enemies, interactions, graphical problems

--- Assets/Scripts/Enemies/BatEnemy.cs
+++ Assets/Scripts/Enemies/BatEnemy.cs
@@ -5,14 +5,6 @@ using UnityEngine.AI;
 
 public class BatEnemy : Enemy
 {
-    Renderer[] renderersRef;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        renderersRef = GetComponentsInChildren<Renderer>();
-    }
-
     public override void TakeDamage(int damage)
     {
         Debug.Log(gameObject.name + " took " + damage + " damage!");
@@ -21,21 +13,8 @@ public class BatEnemy : Enemy
         StartCoroutine(ApplyDamageEffect());
     }
 
-    private IEnumerator ApplyDamageEffect()
+    protected override void Death()
     {
-        foreach (Renderer renderer in renderersRef)
-        {
-            if(renderer != null)
-            {
-                renderer.material.SetColor("_EmissionColor", new Color(0.3f, 0f, 0f));
-                renderer.material.EnableKeyword("_EMISSION");
-            }
-        }
-        yield return new WaitForSeconds(0.5f);
-        foreach (Renderer renderer in renderersRef)
-        {
-            if(renderer != null)
-                renderer.material.DisableKeyword("_EMISSION");
-        }
+        Destroy(gameObject);
     }
 }
--- Assets/Scripts/UI/BossHealthBar.cs
+++ Assets/Scripts/UI/BossHealthBar.cs
@@ -3,19 +3,22 @@ using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
-public class BossHealthBar : BossEnemy
+public class BossHealthBar : BossEnemy //A UI element should not inherit from an Enemy class, but it works so ok
 {
-   
-
     public static int maxhealth;
     public static int currentHealth;
     private Image healthBar;
 
-
-
     void Start()
     {
+        Scene currentScene = SceneManager.GetActiveScene();
+        string sceneName = currentScene.name;
+        if(sceneName != "Boss")
+        {
+            Destroy(gameObject);
+        }
         healthBar = GetComponent<Image>();
         currentHealth = hp;
         maxhealth = hp;
@@ -25,7 +28,6 @@ public class BossHealthBar : BossEnemy
     void Update()
     {
         healthBar.fillAmount = (float)currentHealth / (float)maxhealth;
-
     }
 
 
